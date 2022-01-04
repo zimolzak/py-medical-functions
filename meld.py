555b81609090e9026baaf31089b46b93b889ee49
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-04 14:39:43 -0600

    Non-rounded function, for partial derivative purposes later.

--- meld.py
+++ meld.py
@@ -28,9 +28,14 @@ def meld_i(inr, tb, cr):
             0.643) * 10
 
 
-def meld(na, inr, tb, cr):
+def meld_no_rounding(na, inr, tb, cr):
     m = meld_i(inr, tb, cr)
     n = na_correct(na)
     if m > 11:  # interesting, what if it is 10.6, should it be "pre-rounded"?
         m = m + 1.32 * (137 - n) - (0.033 * m * (137 - n))
+    return m
+
+
+def meld(na, inr, tb, cr):
+    m = meld_no_rounding(na, inr, tb, cr)
     return limit_lab(round(m), hi=40)
