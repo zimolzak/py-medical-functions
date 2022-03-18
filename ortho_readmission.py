bba3e1331a7cc82c3b4f5cdb362bd4ae72a2f496
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-18 14:56:11 -0500

    Using class but lengths wrong

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -1,39 +1,42 @@
 import numpy as np
 import pandas as pd
 
-row_names = [
-    'bias',
-    'age_y',
-    'bmi',
-    'bmi ** 2',
-    '(gender == male)',
-    'dysrhythmia',
-    'heart_failure',
-    'dysrhythmia * heart_failure',
-    'disch_home_or_self',
-    'disch_facility',
-    '(ed_visits > 9)',
-    'psych_dx',
-    'pta_med_count',
-    'drug_abuse_dx',
-    'narcotic_meds',
-    'TJA within past yr',
-]
-
 
 def list_to_dataframe(mylist: list) -> pd.DataFrame:
+    """Any list of length 16, turn it into DataFrame with index in official order. No type checking."""
+    row_names = [
+        'bias',
+        'age_y',
+        'bmi',
+        'bmi ** 2',
+        '(gender == male)',
+        'dysrhythmia',
+        'heart_failure',
+        'dysrhythmia * heart_failure',
+        'disch_home_or_self',
+        'disch_facility',
+        '(ed_visits > 9)',
+        'psych_dx',
+        'pta_med_count',
+        'drug_abuse_dx',
+        'narcotic_meds',
+        'TJA within past yr',
+    ]
     return pd.DataFrame(mylist, index=row_names)
 
 
-def params_to_dataframe(bias: float, age_y: float, bmi: float, gender: float, dysrhythmia: float, heart_failure: float,
-                        discharge: float, ed_visits: float, psych_dx: float, pta_med_count: float,
-                        drug_abuse_dx: float,
-                        narcotic_meds: float,
-                        tja_within_past_yr: float
-                        ) -> pd.DataFrame:
-    mylist = [bias, age_y, bmi, gender, dysrhythmia, heart_failure, discharge, ed_visits, psych_dx, pta_med_count,
-              drug_abuse_dx, narcotic_meds, tja_within_past_yr]
-    return list_to_dataframe(mylist)
+class Model:
+    def __init__(self, bias: float, age_y: float, bmi: float, bmisq, gender: float, dysrhythmia: float,
+                 heart_failure: float, dys_hf_interaction,
+                 discharge_home_self: float, discharge_facility, ed_visits: float, psych_dx: float, pta_med_count: float,
+                 drug_abuse_dx: float,
+                 narcotic_meds: float,
+                 tja_within_past_yr: float):
+        self.as_list = [bias, age_y, bmi, bmisq, gender, dysrhythmia, heart_failure, discharge_home_self,
+                        discharge_facility, psych_dx,
+                        pta_med_count,
+                        drug_abuse_dx, narcotic_meds, tja_within_past_yr]
+        self.as_dataframe = list_to_dataframe(self.as_list)
 
 
 def score(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
@@ -113,19 +116,18 @@ C_list = [
     0.2475,  # drug_abuse_dx
     0.1296,  # narcotic_meds
     -0.3820,  # tja_within_past_12_mo
-
 ]
 
 B_vec = np.array(B_list)
 C_vec = np.array(C_list)
-B_df = pd.DataFrame(B_vec, index=row_names)
-C_df = pd.DataFrame(C_vec, index=row_names)
+B_df = list_to_dataframe(B_list)
+C_df = list_to_dataframe(C_list)
 
 
 class Patient:
     def __init__(self, age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
-             ed_visits: int, psych_dx: bool, pta_med_count: float,
-             drug_abuse_dx: bool, narcotic_meds: bool, tja_within_past_12_mo: bool):
+                 ed_visits: int, psych_dx: bool, pta_med_count: float,
+                 drug_abuse_dx: bool, narcotic_meds: bool, tja_within_past_12_mo: bool):
         # self.age_y = age_y
         # self.bmi = bmi
         # self.gender = gender
@@ -142,20 +144,39 @@ class Patient:
                          heart_failure, discharge == 'home' or discharge == 'self-care', discharge == 'facility',
                          ed_visits > 9, psych_dx, pta_med_count, drug_abuse_dx, narcotic_meds, tja_within_past_12_mo]
 
-    # 'bias',
-    # 'age_y',
-    # 'bmi',
-    # 'bmi ** 2',
-    # '(gender == male)',
-    # 'dysrhythmia',
-    # 'heart_failure',
-    # 'dysrhythmia * heart_failure',
-    # 'disch_home_or_self',
-    # 'disch_facility',
-    # '(ed_visits > 9)',
-    # 'psych_dx',
-    # 'pta_med_count',
-    # 'drug_abuse_dx',
-    # 'narcotic_meds',
-    # 'TJA within past yr',
 
+mod30 = Model(-2.6576,
+              0.0291,
+              -0.1345,
+              0.00218,
+              0.2070,
+              -0.0505,
+              -0.3669,
+              0.7994,
+              -0.3124,
+              0.3645,
+              0.5942,
+              0.1934,
+              0.0332,
+              0,
+              0,
+              0)
+
+mod90 = Model(
+    -0.5527,
+    0,  # age
+    -0.0903,  # bmi
+    0.00145,  # bmi ** 2
+    0.2241,  # (gender == 'male')
+    -0.1169,  # dysrhythmia
+    -0.1284,  # heart_failure
+    0.7544,  # dysrhythmia * heart_failure
+    -0.2464,  # (discharge == 'home' or discharge == 'self-care')
+    0.3233,  # (discharge == 'facility')
+    0.3325,  # (ed_visits > 9)
+    0,  # psych dx
+    0.0193,  # pta_med_count
+    0.2475,  # drug_abuse_dx
+    0.1296,  # narcotic_meds
+    -0.3820  # tja_within_past_12_mo
+)
