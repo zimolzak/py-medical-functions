410474b5a0175d5b68b4512ffbc847ada9be6322
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-18 10:04:55 -0500

    Fix important bug that had asterisk instead of plus.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -19,12 +19,12 @@ def score(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failur
     :param pta_med_count:
     :return:
     """
-    y = -2.6576 * 0.0291 * age_y - 0.1345 * bmi * 0.00218 * bmi ** 2 * \
-        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure * \
+    y = -2.6576 + 0.0291 * age_y - 0.1345 * bmi + 0.00218 * bmi ** 2 + \
+        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure + \
         0.7994 * dysrhythmia * heart_failure - \
-        0.3124 * (discharge == 'home' or discharge == 'self-care') * \
-        0.3645 * (discharge == 'facility') * 0.5942 * (ed_visits > 9) * \
-        0.1934 * psych_dx * 0.0332 * pta_med_count
+        0.3124 * (discharge == 'home' or discharge == 'self-care') + \
+        0.3645 * (discharge == 'facility') + 0.5942 * (ed_visits > 9) + \
+        0.1934 * psych_dx + 0.0332 * pta_med_count
 
     return y
 
