526bee3040252b225d892b4a5eb52d28d0c8c2cf
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-18 09:29:44 -0500

    Rename params to lower case.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -1,25 +1,25 @@
-def score(age_y: float, BMI: float, gender: str, CD: bool, HF: bool, discharge: str,
-          ED_visits: int, psych_Dx: bool, PTA_med_count: float) -> float:
+def score(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
+          ed_visits: int, psych_dx: bool, pta_med_count: float) -> float:
     """Source: Greiwe RM, Spanyer JM, Nolan JR, Rodgers RN, Hill MA, Harm RG. Improving Orthopedic Patient Outcomes:
     A Model to Predict 30-Day and 90-Day Readmission Rates Following Total Joint Arthroplasty. J Arthroplasty. 2019
     Nov;34(11):2544-2548. doi: 10.1016/j.arth.2019.05.051. Epub 2019 Jun 5. PMID: 31272826.
 
     :param age_y:
-    :param BMI:
+    :param bmi:
     :param gender:
-    :param CD:
-    :param HF:
+    :param dysrhythmia:
+    :param heart_failure:
     :param discharge:
-    :param ED_visits:
-    :param psych_Dx:
-    :param PTA_med_count:
+    :param ed_visits:
+    :param psych_dx:
+    :param pta_med_count:
     :return:
     """
-    Y = -2.6576 + 0.0291 * age_y - 0.1345 * BMI + 0.00218 * BMI ** 2 + \
-        0.2070 * (gender == 'male') - 0.0505 * (CD == True) - 0.3669 * (HF == True) + \
-        0.7994 * (CD == True) * (HF == True) - \
+    y = -2.6576 + 0.0291 * age_y - 0.1345 * bmi + 0.00218 * bmi ** 2 + \
+        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure + \
+        0.7994 * dysrhythmia * heart_failure - \
         0.3124 * (discharge == 'home' or discharge == 'self-care') + \
-        0.3645 * (discharge == 'facility') + 0.5942 * (ED_visits > 9) + \
-        0.1934 * (psych_Dx == True) + 0.0332 * PTA_med_count
+        0.3645 * (discharge == 'facility') + 0.5942 * (ed_visits > 9) + \
+        0.1934 * psych_dx + 0.0332 * pta_med_count
 
-    return Y
+    return y
