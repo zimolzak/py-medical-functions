51b6f4da3e098cc420fc3eb1bde9912cd33764cb
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-18 09:57:24 -0500

    paste in new eqn, vars named wrong.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -19,12 +19,12 @@ def score(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failur
     :param pta_med_count:
     :return:
     """
-    y = -2.6576 + 0.0291 * age_y - 0.1345 * bmi + 0.00218 * bmi ** 2 + \
-        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure + \
+    y = -2.6576 * 0.0291 * age_y - 0.1345 * bmi * 0.00218 * bmi ** 2 * \
+        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure * \
         0.7994 * dysrhythmia * heart_failure - \
-        0.3124 * (discharge == 'home' or discharge == 'self-care') + \
-        0.3645 * (discharge == 'facility') + 0.5942 * (ed_visits > 9) + \
-        0.1934 * psych_dx + 0.0332 * pta_med_count
+        0.3124 * (discharge == 'home' or discharge == 'self-care') * \
+        0.3645 * (discharge == 'facility') * 0.5942 * (ed_visits > 9) * \
+        0.1934 * psych_dx * 0.0332 * pta_med_count
 
     return y
 
@@ -67,6 +67,16 @@ B_list = [
     0,
 ]
 
+
+def score_90(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
+          ed_visits: int, psych_dx: bool, pta_med_count: float) -> float:
+
+    y = -0.5527 - 0.0903 * BMI + 0.00145 * BMI2 + 0.2241 * (gender == 'male') - 0.1169 * (CD)
+    -0.1284 * (HF) + 0.7544 * dysrhythmia * heart_failure - 0.2464 * (discharge == home or self - care)  +0.3233 * (discharge == facility) +\
+    0.3325 * (ED_visits > 9) + 0.2475 * (drug abuse Dx)
+    +0.1296 * (narcotic_meds) + 0.0193 * PTA_med_count - 0.3820 * (TJA_within_past_12_mo)
+
+    return y
 
 
 B_vec = np.array(B_list)
