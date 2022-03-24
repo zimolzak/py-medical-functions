6ab2487cbc13282edd811d6b2eeda1c1952324c8
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-24 12:35:32 -0500

    A few renamings.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -40,46 +40,6 @@ class Model:
         self.as_dataframe = list_to_dataframe(self.as_list)
 
 
-def score(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
-          ed_visits: int, psych_dx: bool, pta_med_count: float) -> float:
-    """Source: Greiwe RM, Spanyer JM, Nolan JR, Rodgers RN, Hill MA, Harm RG. Improving Orthopedic Patient Outcomes:
-    A Model to Predict 30-Day and 90-Day Readmission Rates Following Total Joint Arthroplasty. J Arthroplasty. 2019
-    Nov;34(11):2544-2548. doi: 10.1016/j.arth.2019.05.051. Epub 2019 Jun 5. PMID: 31272826.
-
-    :param age_y:
-    :param bmi:
-    :param gender:
-    :param dysrhythmia:
-    :param heart_failure:
-    :param discharge:
-    :param ed_visits:
-    :param psych_dx:
-    :param pta_med_count:
-    :return:
-    """
-    y = -2.6576 + 0.0291 * age_y - 0.1345 * bmi + 0.00218 * bmi ** 2 + \
-        0.2070 * (gender == 'male') - 0.0505 * dysrhythmia - 0.3669 * heart_failure + \
-        0.7994 * dysrhythmia * heart_failure - \
-        0.3124 * (discharge == 'home' or discharge == 'self-care') + \
-        0.3645 * (discharge == 'facility') + 0.5942 * (ed_visits > 9) + \
-        0.1934 * psych_dx + 0.0332 * pta_med_count
-
-    return y
-
-
-def score_90(age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
-             ed_visits: int, psych_dx: bool, pta_med_count: float,
-             drug_abuse_dx, narcotic_meds, tja_within_past_12_mo) -> float:
-    y = -0.5527 - 0.0903 * bmi + 0.00145 * bmi ** 2 + 0.2241 * (gender == 'male') - 0.1169 * dysrhythmia - \
-        0.1284 * heart_failure + 0.7544 * dysrhythmia * heart_failure - \
-        0.2464 * (discharge == 'home' or discharge == 'self-care') + 0.3233 * (discharge == 'facility') + \
-        0.3325 * (ed_visits > 9) + \
-        0.2475 * drug_abuse_dx + \
-        0.1296 * narcotic_meds + 0.0193 * pta_med_count - 0.3820 * tja_within_past_12_mo
-
-    return y
-
-
 class Patient:
     def __init__(self, age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
                  ed_visits: int, psych_dx: bool, pta_med_count: float,
@@ -96,13 +56,13 @@ class Patient:
         # self.drug_abuse_dx = drug_abuse_dx
         # self.narcotic_meds = narcotic_meds
         # self.tja_within_past_12_mo = tja_within_past_12_mo
-        self.x_vector = [1, age_y, bmi, bmi ** 2, gender == 'male', dysrhythmia,
-                         heart_failure, dysrhythmia and heart_failure,
-                         discharge == 'home' or discharge == 'self-care', discharge == 'facility',
-                         ed_visits > 9, psych_dx, pta_med_count, drug_abuse_dx, narcotic_meds, tja_within_past_12_mo]
+        self.as_list = [1, age_y, bmi, bmi ** 2, gender == 'male', dysrhythmia,
+                        heart_failure, dysrhythmia and heart_failure,
+                        discharge == 'home' or discharge == 'self-care', discharge == 'facility',
+                        ed_visits > 9, psych_dx, pta_med_count, drug_abuse_dx, narcotic_meds, tja_within_past_12_mo]
 
 
-mod90 = Model(
+model_90_days = Model(
     -0.5527,
     0,  # age
     -0.0903,  # bmi
@@ -119,39 +79,35 @@ mod90 = Model(
     0.2475,  # drug_abuse_dx
     0.1296,  # narcotic_meds
     -0.3820  # tja_within_past_12_mo
-)
+    )
 
-
-mod30 = Model(-2.6576,
-              0.0291,
-              -0.1345,
-              0.00218,
-              0.2070,
-              -0.0505,
-              -0.3669,
-              0.7994,
-              -0.3124,
-              0.3645,
-              0.5942,
-              0.1934,
-              0.0332,
-              0,
-              0,
-              0)
-
+model_30_days = Model(-2.6576,
+                      0.0291,
+                      -0.1345,
+                      0.00218,
+                      0.2070,
+                      -0.0505,
+                      -0.3669,
+                      0.7994,
+                      -0.3124,
+                      0.3645,
+                      0.5942,
+                      0.1934,
+                      0.0332,
+                      0,
+                      0,
+                      0
+                      )
 
 p = Patient(65, 30, 'male', True, True, 'home', 3, False, 5, False, True, False)
 
-print("Patient")
-print(p.x_vector)
-print(len(p.x_vector))
-print()
+if __name__ == '__main__':
+    print("Patient, length =", len(p.as_list))
+    print(p.as_list)
+    print()
 
-print("Model")
-print(mod30.as_list)
-print(len(mod30.as_list))
-print()
+    print("Model, length =", len(model_30_days.as_list))
+    print(model_30_days.as_list)
+    print()
 
-print(np.dot(p.x_vector, mod30.as_list))
-
-print(score(65, 30, 'male', True, True, 'home', 3, False, 5))
+    print("Score =", np.dot(p.as_list, model_30_days.as_list))
