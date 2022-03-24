8b1e52f1e3ce4739921a87df62b140a66a4e685c
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-24 12:48:30 -0500

    Roll stand-alone function into class. Reformat some line breaks.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -2,48 +2,47 @@ import numpy as np
 import pandas as pd
 
 
-def list_to_dataframe(mylist: list) -> pd.DataFrame:
-    """Any list of length 16, turn it into DataFrame with index in official order. No type checking."""
-    row_names = [
-        'bias',
-        'age_y',
-        'bmi',
-        'bmi ** 2',
-        '(gender == male)',
-        'dysrhythmia',
-        'heart_failure',
-        'dysrhythmia * heart_failure',
-        'disch_home_or_self',
-        'disch_facility',
-        '(ed_visits > 9)',
-        'psych_dx',
-        'pta_med_count',
-        'drug_abuse_dx',
-        'narcotic_meds',
-        'TJA within past yr',
-    ]
-    return pd.DataFrame(mylist, index=row_names)
-
-
 class Model:
-    def __init__(self, bias: float, age_y: float, bmi: float, bmisq, gender: float, dysrhythmia: float,
-                 heart_failure: float, dys_hf_interaction,
-                 discharge_home_self: float, discharge_facility, ed_visits: float, psych_dx: float,
-                 pta_med_count: float,
-                 drug_abuse_dx: float,
-                 narcotic_meds: float,
-                 tja_within_past_yr: float):
+    def __init__(self, bias: float, age_y: float, bmi: float, bmisq,
+                 gender: float, dysrhythmia: float, heart_failure: float, dys_hf_interaction: float,
+                 discharge_home_self: float, discharge_facility: float, ed_visits: float, psych_dx: float,
+                 pta_med_count: float, drug_abuse_dx: float, narcotic_meds: float, tja_within_past_yr: float):
+        """Input regression coefficients as positional or named parameters.
+        Keep them organized and expose list or data frame. Note: 16 coefficients.
+        """
         self.as_list = [bias, age_y, bmi, bmisq, gender, dysrhythmia, heart_failure, dys_hf_interaction,
                         discharge_home_self, discharge_facility, ed_visits, psych_dx,
                         pta_med_count,
                         drug_abuse_dx, narcotic_meds, tja_within_past_yr]
-        self.as_dataframe = list_to_dataframe(self.as_list)
+        row_names = [
+            'bias',
+            'age_y',
+            'bmi',
+            'bmi ** 2',
+            '(gender == male)',
+            'dysrhythmia',
+            'heart_failure',
+            'dysrhythmia * heart_failure',
+            'disch_home_or_self',
+            'disch_facility',
+            '(ed_visits > 9)',
+            'psych_dx',
+            'pta_med_count',
+            'drug_abuse_dx',
+            'narcotic_meds',
+            'TJA within past yr',
+        ]
+        self.as_dataframe = pd.DataFrame(self.as_list, index=row_names)
 
 
 class Patient:
     def __init__(self, age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
                  ed_visits: int, psych_dx: bool, pta_med_count: float,
                  drug_abuse_dx: bool, narcotic_meds: bool, tja_within_past_12_mo: bool):
+        """Input 12 patient characteristics as positional or named parameters.
+        Keep them organized.
+        Expose list of 16 numbers, for multiplying with model coefficients.
+        """
         # self.age_y = age_y
         # self.bmi = bmi
         # self.gender = gender
@@ -56,10 +55,14 @@ class Patient:
         # self.drug_abuse_dx = drug_abuse_dx
         # self.narcotic_meds = narcotic_meds
         # self.tja_within_past_12_mo = tja_within_past_12_mo
-        self.as_list = [1, age_y, bmi, bmi ** 2, gender == 'male', dysrhythmia,
+        self.as_list = [1, age_y,
+                        bmi, bmi ** 2,
+                        gender == 'male', dysrhythmia,
                         heart_failure, dysrhythmia and heart_failure,
                         discharge == 'home' or discharge == 'self-care', discharge == 'facility',
-                        ed_visits > 9, psych_dx, pta_med_count, drug_abuse_dx, narcotic_meds, tja_within_past_12_mo]
+                        ed_visits > 9, psych_dx,
+                        pta_med_count, drug_abuse_dx,
+                        narcotic_meds, tja_within_past_12_mo]
 
 
 model_90_days = Model(
@@ -79,27 +82,16 @@ model_90_days = Model(
     0.2475,  # drug_abuse_dx
     0.1296,  # narcotic_meds
     -0.3820  # tja_within_past_12_mo
-    )
+)
 
-model_30_days = Model(-2.6576,
-                      0.0291,
-                      -0.1345,
-                      0.00218,
-                      0.2070,
-                      -0.0505,
-                      -0.3669,
-                      0.7994,
-                      -0.3124,
-                      0.3645,
-                      0.5942,
-                      0.1934,
-                      0.0332,
-                      0,
-                      0,
-                      0
-                      )
+model_30_days = Model(-2.6576, 0.0291, -0.1345, 0.00218,
+                      0.2070, -0.0505, -0.3669, 0.7994,
+                      -0.3124, 0.3645, 0.5942, 0.1934,
+                      0.0332, 0, 0, 0)
 
-p = Patient(65, 30, 'male', True, True, 'home', 3, False, 5, False, True, False)
+p = Patient(65, 30, 'male', True,
+            True, 'home', 3, False,
+            5, False, True, False)
 
 if __name__ == '__main__':
     print("Patient, length =", len(p.as_list))
