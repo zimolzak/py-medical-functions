0283f92e6c5faf986f1474c4ae5d033b69384885
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-24 13:17:38 -0500

    Shape up some comma items more sensibly like 4x4.

--- ortho_readmission.py
+++ ortho_readmission.py
@@ -10,35 +10,21 @@ class Model:
         """Input regression coefficients as positional or named parameters.
         Keep them organized and expose list or data frame. Note: 16 coefficients.
         """
-        self.as_list = [bias, age_y, bmi, bmisq, gender, dysrhythmia, heart_failure, dys_hf_interaction,
+        self.as_list = [bias, age_y, bmi, bmisq,
+                        gender, dysrhythmia, heart_failure, dys_hf_interaction,
                         discharge_home_self, discharge_facility, ed_visits, psych_dx,
-                        pta_med_count,
-                        drug_abuse_dx, narcotic_meds, tja_within_past_yr]
-        row_names = [
-            'bias',
-            'age_y',
-            'bmi',
-            'bmi ** 2',
-            '(gender == male)',
-            'dysrhythmia',
-            'heart_failure',
-            'dysrhythmia * heart_failure',
-            'disch_home_or_self',
-            'disch_facility',
-            '(ed_visits > 9)',
-            'psych_dx',
-            'pta_med_count',
-            'drug_abuse_dx',
-            'narcotic_meds',
-            'TJA within past yr',
-        ]
+                        pta_med_count, drug_abuse_dx, narcotic_meds, tja_within_past_yr]
+        row_names = ['bias', 'age_y', 'bmi', 'bmi ** 2',
+                     '(gender == male)', 'dysrhythmia', 'heart_failure', 'dysrhythmia * heart_failure',
+                     'disch_home_or_self', 'disch_facility', '(ed_visits > 9)', 'psych_dx',
+                     'pta_med_count', 'drug_abuse_dx', 'narcotic_meds', 'TJA within past yr']
         self.as_dataframe = pd.DataFrame(self.as_list, index=row_names)
 
 
 class Patient:
-    def __init__(self, age_y: float, bmi: float, gender: str, dysrhythmia: bool, heart_failure: bool, discharge: str,
-                 ed_visits: int, psych_dx: bool, pta_med_count: float,
-                 drug_abuse_dx: bool, narcotic_meds: bool, tja_within_past_12_mo: bool):
+    def __init__(self, age_y: float, bmi: float, gender: str, dysrhythmia: bool,
+                 heart_failure: bool, discharge: str, ed_visits: int, psych_dx: bool,
+                 pta_med_count: float, drug_abuse_dx: bool, narcotic_meds: bool, tja_within_past_12_mo: bool):
         """Input 12 patient characteristics as positional or named parameters.
         Keep them organized.
         Expose list of 16 numbers, for multiplying with model coefficients.
