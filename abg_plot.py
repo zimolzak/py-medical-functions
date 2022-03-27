c1d54ec9fedd936ed32a4c803efe67ed9f9a8e45
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-27 09:28:32 -0500

    This is weirdly beautiful but wrong. What is it?

--- abg_plot.py
+++ abg_plot.py
@@ -6,22 +6,40 @@ from abg_data import REGION_DICT
 L = ["I don't know"] + list(REGION_DICT.keys())
 print(L)
 
-test_space = np.linspace([7.0, 0], [7.8, 56], num=500)
+x, y = np.mgrid[7.0:7.8:0.02, 0:56:1]
+test_space = np.vstack((x.ravel(), y.ravel()))
+test_space = test_space.transpose()
+
 answer_space = []
 for lab in test_space:
     ph, bicarb = lab
-    s = interpret_as_str(ph, bicarb)
+    try:
+        s = interpret_as_str(ph, bicarb)
+    except ValueError as e:
+        print(ph, bicarb, e)
     i = L.index(s)
     answer_space.append(i)
 
-
-
 A = np.array(answer_space)
-# print(A)
 
-over = test_space[:,0] > 7.35
-under = test_space[:,0] < 7.45
-v = np.vstack((over, under))
-conjunction = np.all(v, axis=0)
-found = test_space[np.nonzero(conjunction), :]
-print(found)
+for i in range(len(L)):
+    indices = np.nonzero(A == i)
+    yi = test_space[indices, 1]
+    xi = test_space[indices, 0]
+    plt.plot(xi, yi, 'o')
+    # print(i)
+    # print(np.argwhere(A == i))
+    # print()
+    # print()
+
+plt.savefig('outoutout.png')
+
+
+#  print(A)
+
+# over = test_space[:, 0] > 7.35
+# under = test_space[:, 0] < 7.45
+# v = np.vstack((over, under))
+# conjunction = np.all(v, axis=0)
+# found = test_space[np.nonzero(conjunction), :]
+# print(found)
